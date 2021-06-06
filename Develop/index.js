3d628c08f6aa37925f17d64e130e683af717e887
Edgar Lule <edgar21lc@gmail.com>
2021-06-05 18:45:18 -0700

    added html page

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,16 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const Manager = require('./lib/Manager')
-const Engineer = require('./lib/Engineer')
-const Intern = require('./lib/Intern')
-const path = require('path');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+// const path = require('path');
 
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+// const OUTPUT_DIR = path.resolve(__dirname, "output");
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-// const createHTML = require('./lib/createHTML') // this this to pass in an a premade HTML/Styled page
-const render = require("./lib/createHTML");
+const cHTML = require('./lib/createHTML'); // this this to pass in an a premade HTML/Styled page
+// const render = require("./lib/createHTML");
 
 const team = []; // array for team
 
@@ -170,11 +170,11 @@ const addNewEmployee = () => {
       if (answer.newMember) {
         employeeType();
       } else {
-        const renderTeam = render(team); // gets the team array and // cant use this havent leanred react
-        // let htmlPage = createHTML(team); // i think this works
+        // const renderTeam = render(team); // gets the team array and // cant use this havent leanred react
+        const htmlPage = cHTML(team); // i think this works
         console.log(htmlPage);
-        fs.writeFile(outputPath, renderTeam);
-        // fs.writeFile("teamTest.html", htmlPage); // i think this works
+        // fs.writeFile(outputPath, renderTeam);
+        fs.writeFile("team(Testing).html", htmlPage, () => {}); // i think this works
         console.log("A file containing your team has been created.");
       }
     })
--- Develop/lib/createHTML.js
+++ Develop/lib/createHTML.js
@@ -1,14 +1,125 @@
 function createHTML(team) {
-    var Manager = ``;
-    var Engineer = ``;
-    var Intern = ``;
-    team.array.forEach(employee => {
-        if(employee.role == 'Manager') {
+  let createdManager = ``;
+  let createdEngineer = ``;
+  let createdIntern = ``;
+  team.forEach((employee) => {
+    if (employee.role == "Manager") {
+      createdManager =
+        createdManager +
+        `
+            <!-- Manager -->
+            <div class="col-12 col-md-3 offset-md-1 border">
+              <div class="bg-secondary p-3 header">
+                <h2>Name: ${employee.name}</h2>
+                <h3>Role: ${employee.role}</h3>
+              </div>
+              <div class="bottom">
+                <p>Id: ${employee.id}</p>
+                <p>Office Number: ${employee.officeNumber}</p>
+                <a href="mailto:${employee.email}" class="card-link emailLink">email</a>
+              </div>
+            </div>`;
+    } // end of if statment
 
-        } // end of if statment
-    }); // end of for each loop
-
+    if (employee.role == "Engineer") {
+      createdEngineer =
+        createdEngineer +
+        ` 
+        <!-- Engineer -->
+        <div class="col-12 col-md-3 offset-md-1 border">
+           <div class="bg-success p-3 header">
+             <h2>Name: ${employee.name}</h2>
+             <h3>Role: ${employee.role}</h3>
+           </div>
+           <div class="bottom">
+             <p>Id: ${employee.id}</p>
+             <a>Gitub: https://github.com/${employee.github}</a><br /><br />
+             <a href="mailto:${member.email}" class="card-link emailLink">email</a>
+           </div>
+         </div>`;
+    } // end  of if statment
+    if (employee.role == "Intern") {
+      createdIntern =
+        createdIntern +
+        ` 
+        <!-- Intern -->
+        <div class="col-12 col-md-3 offset-md-1 border">
+          <div class="bg-info p-3 header">
+            <h2>Name: ${employee.name}</h2>
+            <h3>Role: ${employee.role}</h3>
+          </div>
+          <div class="bottom">
+            <p>Id: ${employee.id}</p>
+            <p>School: ${employee.school}</p>
+            <a href="mailto:${member.email}" class="card-link emailLink">email</a>
+          </div>
+        </div>`;
+    }
+  });
+  return (
+    `
+    <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>GenerateHTML</title>
+      <link
+        rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossorigin="anonymous"
+      />
+      <link rel="stylesheet" href="style.css" />
+    </head>
+    <body>
+      <header>
+        <div class="p-3 mb-4 bg-primary rounded-3">
+          <div class="h-100 p-3 text-white text-center">
+            <h1>Welcome To Your Team</h1>
+          </div>
+        </div>
+      </header>
+  
+      <!-- <div class="container"> -->
+      <!-- <div class="container"> -->
+      <div class="row">
+        <!-- Manager -->
+      ` +
+    createdManager +
+    `
+        <!-- Engineer -->
+      ` +
+    createdEngineer +
+    `
+        <!-- Intern -->
+      ` +
+    createdIntern +
+    `
+      </div>
+      <script
+        src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"
+      ></script>
+      <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"
+      ></script>
+      <script
+        src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"
+      ></script>
+      <script src="../index.js"></script>
+    </body>
+  </html>`
+  ); // end of for each loop
 }
+
+module.exports = createHTML;
 
 // const path = require("path");
 // const fs = require("fs");
